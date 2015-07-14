6e4d5c4338fa0d38ee30aa226727cdfe26a3521e
Hersh Amin <h.amin@utexas.edu>
2015-07-13 22:07:20 -0500

    Bug fix in event request JSON schema for mongoose

--- Schema/eventRequest.js
+++ Schema/eventRequest.js
@@ -2,14 +2,14 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var requestSchema = new Schema({
-	city: {type:String, required'Missing City Name'},
-	state: {type:String, required'Missing State Abbr.'},
-	zip: {type:Number, required'Missing Zip Code'},
-	email: {type:String, required'Missing Email'},
-	name: {type:String, required'Missing Name'},
-	latlng: {type:String, required'Missing Request Coordinates'},
-	did: {type:String, required'Missing Unique Device ID'},
-	timestamp: {type:Number, required'Missing Timestamp in EPOCH'}
+	city: {type:String, required:'Missing City Name'},
+	state: {type:String, required:'Missing State Abbr.'},
+	zip: {type:Number, required:'Missing Zip Code'},
+	email: {type:String, required:'Missing Email'},
+	name: {type:String, required:'Missing Name'},
+	latlng: {type:String, required:'Missing Request Coordinates'},
+	did: {type:String, required:'Missing Unique Device ID'},
+	timestamp: {type:Number, required:'Missing Timestamp in EPOCH'}
 });
 
 module.exports = mongoose.model('EventRequest', requestSchema)
