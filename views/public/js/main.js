47dc0e2d8ad135647ed70a3b024748d6a93f53dc
Aaron Dack <adackbusiness@gmail.com>
2015-07-16 16:13:43 -0500

    adding controller

--- views/public/js/main.js
+++ views/public/js/main.js
@@ -72,3 +72,15 @@ app.controller('scrapeController', function($scope, $modal, $http) {
 	}
 	
 });
+
+app.controller('HeatmapController', function($scope, $http) {
+		// Fetch complete list from website
+		var response = $http.get('/requests');
+		response.success(function(res){
+			$scope.heatmap = res;
+		});
+		response.error(function(res){
+			alert("Failure: " + res);
+		});
+});
+
