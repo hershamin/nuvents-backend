759f9c0541bb6040b2605dc2bf6cc5f567b19838
hersh <h.amin@utexas.edu>
2015-08-15 14:16:54 -0500

    Removed _id field from selector array in website detail schema

--- Schema/websiteDetail.js
+++ Schema/websiteDetail.js
@@ -1,6 +1,15 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
+// Website selector sub schema, prevent _id from being created
+var selectorSubSchema = new Schema({
+	selector: String,
+	htmlAttr: String,
+	variable: String,
+	array: Boolean,
+	html: Boolean
+}, {_id:false});
+
 var detailSchema = new Schema({
 	wid: {type:Number, required:'Missing Website ID'},
 	startWebsite: {type:String, required:'Missing Start Website'},
@@ -8,13 +17,7 @@ var detailSchema = new Schema({
 		url: String
 	}],
 	crawlerRegEx: {type:String, required:'Missing Crawler Regular Expression'},
-	selectors: [{
-		selector: String,
-		htmlAttr: String,
-		variable: String,
-		array: Boolean,
-		html: Boolean
-	}],
+	selectors: [selectorSubSchema],
 	eventAttr: String,
 	jsEval: String,
 	eid: [String]
